5b1c5f5ab85fb23d4aa9a5013737bac4e81f20d5
mahmud035 <mahamudulhasan4148@gmail.com>
2022-12-10 23:23:08 +0600

    use react-photo-view to see the full project screenshot

--- src/Components/Home/ProjectCard/ProjectCard.js
+++ src/Components/Home/ProjectCard/ProjectCard.js
@@ -3,6 +3,7 @@ import { Button } from 'react-bootstrap';
 import './ProjectCard.css';
 import { FaCode } from 'react-icons/fa';
 import { FiExternalLink } from 'react-icons/fi';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
 
 const ProjectCard = ({ project }) => {
   const {
@@ -16,72 +17,78 @@ const ProjectCard = ({ project }) => {
   } = project;
 
   return (
-    <div>
-      <div className="project-card card h-100  border-0 shadow">
-        <div className="row g-2">
-          <div className="col-12 ">
-            <img
-              src={image}
-              className="project-image rounded"
-              alt="Project Demo"
-            />
-          </div>
-          <div className="col-12">
-            <div className="card-body">
-              <h2 className="card-title">{name}</h2>
-              <div>
-                {descriptions.map((bulletPoint, index) => (
-                  <div className="d-flex">
-                    <li></li>
-                    <p key={index}>{bulletPoint}</p>
-                  </div>
-                ))}
-              </div>
+    <PhotoProvider>
+      <div>
+        <div className="project-card card h-100  border-0 shadow">
+          <div className="row g-2">
+            <div className="col-12 ">
+              <PhotoView src={image}>
+                <img
+                  src={image}
+                  className="project-image rounded"
+                  alt="Project Demo"
+                />
+              </PhotoView>
+            </div>
+            <div className="col-12">
+              <div className="card-body">
+                <h2 className="card-title">{name}</h2>
+                <div>
+                  {descriptions.map((bulletPoint, index) => (
+                    <div className="d-flex">
+                      <li></li>
+                      <p key={index}>{bulletPoint}</p>
+                    </div>
+                  ))}
+                </div>
 
-              <div className="all-technology ">
-                {technologyUsed.map((technology, index) => (
-                  <>
-                    <span className="fw-semibold technology" key={index}>
-                      {technology}
-                    </span>
-                  </>
-                ))}
-              </div>
+                <div className="all-technology ">
+                  {technologyUsed.map((technology, index) => (
+                    <>
+                      <span className="fw-semibold technology" key={index}>
+                        {technology}
+                      </span>
+                    </>
+                  ))}
+                </div>
 
-              <div className="py-4 d-flex flex-wrap gap-2">
-                <a href={liveSite} target="_blank" rel="noreferrer">
-                  <Button variant="outline-success" className="fw-semibold">
-                    Live Website
-                    <FiExternalLink
-                      size={22}
-                      style={{ marginBottom: '2px', marginLeft: '5px' }}
-                    />
-                  </Button>
-                </a>
-                <a href={gitHubClient} target="_blank" rel="noreferrer">
-                  <Button variant="outline-success" className="fw-semibold">
-                    Github Client
-                    <FaCode
-                      size={22}
-                      style={{ marginBottom: '2px', marginLeft: '5px' }}
-                    />
-                  </Button>
-                </a>
-                <a href={gitHubServer} target="_blank" rel="noreferrer">
-                  <Button variant="outline-success" className="fw-semibold">
-                    Github Server
-                    <FaCode
-                      size={22}
-                      style={{ marginBottom: '2px', marginLeft: '5px' }}
-                    />
-                  </Button>
-                </a>
+                <div className="py-4 d-flex flex-wrap gap-2">
+                  <a href={liveSite} target="_blank" rel="noreferrer">
+                    <Button variant="outline-success" className="fw-semibold">
+                      Live Website
+                      <FiExternalLink
+                        size={22}
+                        style={{ marginBottom: '2px', marginLeft: '5px' }}
+                      />
+                    </Button>
+                  </a>
+                  <a href={gitHubClient} target="_blank" rel="noreferrer">
+                    <Button variant="outline-success" className="fw-semibold">
+                      Github Client
+                      <FaCode
+                        size={22}
+                        style={{ marginBottom: '2px', marginLeft: '5px' }}
+                      />
+                    </Button>
+                  </a>
+                  {gitHubServer && (
+                    <a href={gitHubServer} target="_blank" rel="noreferrer">
+                      <Button variant="outline-success" className="fw-semibold">
+                        Github Server
+                        <FaCode
+                          size={22}
+                          style={{ marginBottom: '2px', marginLeft: '5px' }}
+                        />
+                      </Button>
+                    </a>
+                  )}
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </PhotoProvider>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import 'react-photo-view/dist/react-photo-view.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
